df97836bcffaebb491f38f9c2d7e4b0dfe2e93c6
your_username <malahat.m82@gmail.com>
2025-03-17 16:58:33 +0100

    Header is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
-
+import Header from "./components/Header"
 
 function App() {
   
 
   return (
     <>
-     <h1>Contact App</h1> 
+    <Header/>
+
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+import styles from "./Header.module.css"
+
+function Header() {
+  return (
+	<div className={styles.container}>
+		<h1>Contact App</h1>
+		<p>
+			<a>
+				Malahat
+			</a> | First React Project
+		</p>
+
+</div>
+  )
+}
+
+export default Header
