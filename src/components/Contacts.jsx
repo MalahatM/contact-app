9e81857e71dc256b69529f391948de3bd4b2c5ed
your_username <malahat.m82@gmail.com>
2025-03-19 12:56:50 +0100

    Shorten and clean up codes.

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import ContactsList from './ContactsList';
+import inputs from "../constants/inputs";
 
 
 function Contacts() {
@@ -30,10 +31,18 @@ function Contacts() {
   return (
 	<div>
 		<div>
-		<input type="text" placeholder="Name" name="name" value={contact.name} onChange={changeHandler}/>
-		<input type="text" placeholder="Last Name" name="lastName" value={contact.lastName} onChange={changeHandler}/>
-		<input type="email" placeholder="Email" name="email" value={contact.email} onChange={changeHandler}/>
-		<input type="number" placeholder="Phone" name="phone" value={contact.phone} onChange={changeHandler}/>
+			{
+				inputs.map((input, index)=>
+					(<input
+					key={index}
+					type={input.type}
+					placeholder={input.placeholder}
+					name={input.name}
+					value={contact[input.name]}
+					onChange={changeHandler}
+					/>
+				))
+			}
 		<button onClick={addHandler}>Add Contacts</button>
 		</div>
 		<div>
